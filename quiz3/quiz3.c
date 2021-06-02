af1f890e81625872d4fce301e9f5af995c30cb0b
Jake Underland <jaked0626@cs.uchicago.edu>
2021-06-02 13:00:28 -0500

    first draft

--- quiz3/quiz3.c
+++ quiz3/quiz3.c
@@ -7,6 +7,8 @@
 struct htbl {
     unsigned int size, occupied;
     char** entries; /* an array of strings */
+    pthread_mutex_t m;
+    pthread_cond_t wait;
 };
 
 typedef struct htbl htbl;
@@ -21,6 +23,9 @@ htbl* new_htbl(unsigned int size) {
     res->occupied = 0;
     
     res->entries = (char**)malloc(sizeof(char*) * size);
+
+    pthread_mutex_init(&res->m, NULL);
+    pthread_cond_init(&res->wait, NULL);
     
     /* each entry starts off empty */
     unsigned int i;
@@ -56,10 +61,11 @@ void insert(htbl* ht, char* str) {
     /* assumptions: string is not already in table */
     
     unsigned int i = hash(str, ht->size);
-    
-    if (ht->occupied + 1 >= ht->size) {
-        fprintf(stderr, "insert to full htbl\n");
-        exit(1);
+
+    pthread_mutex_lock(&ht->m);
+    while (ht->occupied + 1 >= ht->size) { //changed to while so no race condition
+        pthread_cond_wait(&ht->wait, &ht->m);
+        // exit(1);
     }
 
     /* will use the first empty or empty-from-removal spot found */
@@ -69,6 +75,7 @@ void insert(htbl* ht, char* str) {
     
     ht->entries[i] = str;
     ht->occupied++;
+    pthread_mutex_unlock(&ht->m);
 }
 
 
@@ -77,6 +84,7 @@ void htbl_remove(htbl* ht, char* str) {
     
     unsigned int i = hash(str, ht->size);
     
+    pthread_mutex_lock(&ht->m);
     /* note: strcmp returns zero when two strings match */
     while (strcmp(ht->entries[i], str) != 0) {
         i = (i + 1) % ht->size;
@@ -84,14 +92,18 @@ void htbl_remove(htbl* ht, char* str) {
     
     ht->entries[i] = ""; /* empty-from-removal denoted by empty string */
     ht->occupied--;
+    pthread_mutex_unlock(&ht->m);
+    pthread_cond_signal(&ht->wait);
 }
 
 
 bool member(htbl* ht, char* str) {
     unsigned int i = hash(str, ht->size);
     unsigned int start = i; /* first spot searched, to avoid cycling forever */
+    bool res = false;
     
     /* note: strcmp returns zero when two strings match */
+    pthread_mutex_lock(&ht->m);
     while (ht->entries[i] != NULL && strcmp(ht->entries[i], str) != 0) {
         i = (i + 1) % ht->size;
         
@@ -99,11 +111,18 @@ bool member(htbl* ht, char* str) {
          * without encountering string or NULL, cannot be in table;
          * avoid infinite loop */
         if (i == start) {
+            pthread_mutex_unlock(&ht->m);
             return false;
         }
     }
-    
+
     /* did loop halt due to empty spot or string match? */
     /* string matched if not empty spot */
-    return ht->entries[i] != NULL;
+    // added conditional to ensure value of ith entry does not change before return
+    if (ht->entries[i] != NULL) {
+        res = true;
+    }
+    pthread_mutex_unlock(&ht->m); 
+
+    return res;
 }
